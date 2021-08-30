93c7a6f12da7d75a03cfd8342fe35a421d36dd13
Ramildo82 <ramildo82@gmail.com>
2021-08-30 14:52:08 -0300

    Inicio

---
+++ CadastroSerie/CadastroSerie/Classes/EntidadesBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroSerie
+{
+    public abstract class EntidadesBase
+    {
+        public int Id { get; protected set; }
+    }
+}
